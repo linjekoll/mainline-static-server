9020282842f91a86af9c47eee38f33fb7aaea38f
Linus Oleander <linus@oleander.nu>
2011-10-19 18:14:45 +0200

    Server should be able to respond to /lines

--- server.rb
+++ server.rb
@@ -17,8 +17,13 @@ get "/lines/:line_id/stops" do
 end
 
 get "/lines/:line_id" do
-  puts "/lines/#{params["line_id"]}"
-  static("static/lines.json").from_json[params["line_id"]].to_json
+  static("static/lines.json").from_json["4"].to_json
+end
+
+get "/lines" do
+  (params[:amount] || 30).to_i.times.map do |n|
+    static("static/lines.json").from_json["4"].merge("id" => n + 1)
+  end.to_json
 end
 
 get "/" do
